959081c894b5c6350673cf5ceedc0e10f467b51e
BIG"Opinion <114103606+cybermexanik@users.noreply.github.com>
2024-09-27 12:22:06 +0300

    Add files via upload

---
+++ nike-main/src/variables/index.js
@@ -0,0 +1,145 @@
+import {
+  facebook,
+  instagram,
+  shieldTick,
+  support,
+  truckFast,
+  twitter,
+} from "../assets/icons/index.js";
+import {
+  bigShoe1,
+  bigShoe2,
+  bigShoe3,
+  customer2,
+  shoe4,
+  shoe5,
+  shoe6,
+  shoe7,
+  thumbnailShoe1,
+  thumbnailShoe2,
+  thumbnailShoe3,
+} from "../assets/images/index.js";
+
+export const navLinks = [
+  { href: "#home", label: "Home" },
+  { href: "#about-us", label: "About Us" },
+  { href: "#products", label: "Products" },
+  { href: "#contact-us", label: "Contact Us" },
+];
+
+export const shoes = [
+  {
+    thumbnail: thumbnailShoe1,
+    bigShoe: bigShoe1,
+  },
+  {
+    thumbnail: thumbnailShoe2,
+    bigShoe: bigShoe2,
+  },
+  {
+    thumbnail: thumbnailShoe3,
+    bigShoe: bigShoe3,
+  },
+];
+
+export const statistics = [
+  { value: "1k+", label: "Brands" },
+  { value: "500+", label: "Shops" },
+  { value: "250k+", label: "Customers" },
+];
+
+export const products = [
+  {
+    imgURL: shoe4,
+    name: "Nike Air Jordan-01",
+    price: "7299₽",
+  },
+  {
+    imgURL: shoe5,
+    name: "Nike Air Jordan-10",
+    price: "5699₽",
+  },
+  {
+    imgURL: shoe6,
+    name: "Nike Air",
+    price: "8999₽",
+  },
+  {
+    imgURL: shoe7,
+    name: "Nike Air Jordan-001",
+    price: "6299₽",
+  },
+];
+
+export const services = [
+  {
+    imgURL: truckFast,
+    label: "Free shipping",
+    subtext: "Enjoy seamless shopping with our complimentary shipping service.",
+  },
+  {
+    imgURL: shieldTick,
+    label: "Secure Payment",
+    subtext:
+      "Experience worry-free transactions with our secure payment options.",
+  },
+  {
+    imgURL: support,
+    label: "Love to help you",
+    subtext: "Our dedicated team is here to assist you every step of the way.",
+  },
+];
+
+export const reviews = [
+  {
+    imgURL: customer2,
+    customerName: "Andrew Antonov",
+    rating: 4.7,
+    feedback:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque luctus posuere!",
+  },
+  {
+    imgURL: customer2,
+    customerName: "Andrew Antonov",
+    rating: 4.5,
+    feedback:
+      "Nullam at diam pretium, iaculis augue nec, molestie lacus. Nam sagittis urna nec arcu aliquam aliquam. !",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Products",
+    links: [
+      { name: "Air Force 1", link: "/" },
+      { name: "Air Max 1", link: "/" },
+      { name: "Air Jordan 1", link: "/" },
+      { name: "Air Force 2", link: "/" },
+      { name: "Nike Waffle Racer", link: "/" },
+      { name: "Nike Cortez", link: "/" },
+    ],
+  },
+  {
+    title: "Help",
+    links: [
+      { name: "About us", link: "/" },
+      { name: "FAQs", link: "/" },
+      { name: "How it works", link: "/" },
+      { name: "Privacy policy", link: "/" },
+      { name: "Payment policy", link: "/" },
+    ],
+  },
+  {
+    title: "Get in touch",
+    links: [
+      { name: "customer@nike.com", link: "mailto:customer@nike.com" },
+      { name: "+639959813002", link: "mobile:+639959813002" },
+    ],
+  },
+];
+
+export const socialMedia = [
+  { src: facebook, alt: "facebook logo" },
+  { src: twitter, alt: "twitter logo" },
+  { src: instagram, alt: "instagram logo" },
+];
